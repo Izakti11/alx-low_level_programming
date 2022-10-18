06c06bbd8a7f0f1e7dcb88489b348a16810cbcec
izakti11 <izaktilahun11@gmail.com>
2022-10-18 16:07:21 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 int _isalpha(int c);
 int _islower(int c);
 int _putchar(char c);
